34da65db9f84d3e5a42505e1b7278dbfa848395d
junghoon <greenjm1@nate.com>
2021-07-14 21:34:55 +0900

    5일차

--- mdtest/polls/views.py
+++ mdtest/polls/views.py
@@ -2,28 +2,12 @@ from django.http import HttpResponse, HttpResponseRedirect
 #from django.http.response import Http404
 from django.shortcuts import get_object_or_404, render
 #from django.template import loader
-from django.shortcuts import render
+#from django.shortcuts import render
 from django.urls import reverse
+from django.views import generic
 
 from .models import Choice, Question
 
-
-'''
-#simple render way
-def index(request):
-    latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    context = {'latest_question_list': latest_question_list}
-    return render(request, 'polls/index.html', context)
-
-#simple 404 error boom way
-def detail(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'polls/detail.html', {'question': question})
-
-def results(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'polls/results.html', {'question': question})
-'''
 #Generic view
 class IndexView(generic.ListView):
     template_name = 'polls/index.html'

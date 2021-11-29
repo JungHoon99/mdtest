5173318d3e7d6780b27f8eb34224266a27629db8
JungHoon99 <greenjm1@nate.comcom>
2021-11-29 19:14:20 +0900

    6일차 공부

--- mdtest/polls/test.py
+++ mdtest/polls/test.py
@@ -10,8 +10,8 @@ class QuestionModelTests(TestCase):
 
     def test_was_published_recently_with_future_question(self):
         """
-        was_published_recently() returns False for questions whose pub_date
-        is in the future.
+        was_published_recently()는 
+        pub_date가 미래인 질문에 대해 False를 반환합니다.
         """
         time = timezone.now() + datetime.timedelta(days=30)
         future_question = Question(pub_date=time)

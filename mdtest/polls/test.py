e2f37e548d108580999a70696117ded87a57a241
JungHoon99 <greenjm1@nate.com>
2021-11-30 22:16:02 +0900

    7일차 테스트 해석

--- mdtest/polls/test.py
+++ mdtest/polls/test.py
@@ -37,9 +37,10 @@ class QuestionModelTests(TestCase):
 
 def create_question(question_text, days):
     """
-    Create a question with the given `question_text` and published the
-    given number of `days` offset to now (negative for questions published
-    in the past, positive for questions that have yet to be published).
+    주어진 'question_text'로 질문을 만들고 주어진 
+    'days' 오프셋을 지금까지 게시했습니다
+    (과거에 게시된 질문의 경우 음수, 
+    아직 게시되지 않은 질문의 경우 양수).
     """
     time = timezone.now() + datetime.timedelta(days=days)
     return Question.objects.create(question_text=question_text, pub_date=time)
@@ -47,7 +48,7 @@ def create_question(question_text, days):
 class QuestionIndexViewTests(TestCase):
     def test_no_questions(self):
         """
-        If no questions exist, an appropriate message is displayed.
+        질문이 없으면 해당 메시지가 표시됩니다.
         """
         response = self.client.get(reverse('polls:index'))
         self.assertEqual(response.status_code, 200)
@@ -56,8 +57,8 @@ class QuestionIndexViewTests(TestCase):
 
     def test_past_question(self):
         """
-        Questions with a pub_date in the past are displayed on the
-        index page.
+        과거에 pub_date가 있는 질문은 
+        색인 페이지에 표시됩니다.
         """
         question = create_question(question_text="Past question.", days=-30)
         response = self.client.get(reverse('polls:index'))
@@ -78,8 +79,8 @@ class QuestionIndexViewTests(TestCase):
 
     def test_future_question_and_past_question(self):
         """
-        Even if both past and future questions exist, only past questions
-        are displayed.
+        과거 질문과 미래 질문이 모두 
+        존재하더라도 과거 질문만 표시됩니다.
         """
         question = create_question(question_text="Past question.", days=-30)
         create_question(question_text="Future question.", days=30)
@@ -91,7 +92,7 @@ class QuestionIndexViewTests(TestCase):
 
     def test_two_past_questions(self):
         """
-        The questions index page may display multiple questions.
+        질문 색인 페이지에는 여러 질문이 표시될 수 있습니다.
         """
         question1 = create_question(question_text="Past question 1.", days=-30)
         question2 = create_question(question_text="Past question 2.", days=-5)
@@ -104,8 +105,8 @@ class QuestionIndexViewTests(TestCase):
 class QuestionDetailViewTests(TestCase):
     def test_future_question(self):
         """
-        The detail view of a question with a pub_date in the future
-        returns a 404 not found.
+        미래의 pub_date가 있는 질문의 세부 정보 보기는 
+        404 not found를 반환합니다.
         """
         future_question = create_question(question_text='Future question.', days=5)
         url = reverse('polls:detail', args=(future_question.id,))
@@ -114,8 +115,8 @@ class QuestionDetailViewTests(TestCase):
 
     def test_past_question(self):
         """
-        The detail view of a question with a pub_date in the past
-        displays the question's text.
+        과거에 pub_date가 있는 질문의 세부 정보 보기에는 
+        질문의 텍스트가 표시됩니다.
         """
         past_question = create_question(question_text='Past Question.', days=-5)
         url = reverse('polls:detail', args=(past_question.id,))

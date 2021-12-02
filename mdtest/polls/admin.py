a90d322c73767cbaecc02914f31250e076a8aef1
JungHoon99 <greenjm1@nate.com>
2021-12-02 20:14:28 +0900

    8일차-1

--- mdtest/polls/admin.py
+++ mdtest/polls/admin.py
@@ -2,5 +2,8 @@ from django.contrib import admin
 
 from .models import Question
 
+class QuestionAdmin(admin.ModelAdmin):
+    fileds = ['pub_date', 'question_text']
+
 admin.site.register(Question)
 # Register your models here.
